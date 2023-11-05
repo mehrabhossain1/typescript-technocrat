2d42620e60583df1337af4759459e8587ebb1f27
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-11-05 22:02:00 +0600

    learnt generic with type

--- module2/2.3.ts
+++ module2/2.3.ts
@@ -8,11 +8,31 @@
   //   const boolArr: boolean[] = [true, false, true];
   const boolArr: Array<boolean> = [true, false, true];
 
-  type GenericArray<param> = Array<param>;
+  type GenericArray<T> = Array<T>;
+
   //   const rollNums : GenericArray<number> = [2,3,4, '2']
   //   const stNames: GenericArray<string> = ["M", "n", 2];
   const ids: GenericArray<number> = [2, 3, 4, 5];
 
   type GenericArray2<param> = Array<param>;
   const contacts: GenericArray2<string> = ["st", "ss"];
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "Mehrab",
+      age: 23,
+    },
+  ];
+
+  // generic tuple
+  type TGenericTuple<X, Y> = [X, Y];
+  const manush: TGenericTuple<string, number> = ["Mr. X", 23];
+
+  type TGenericTuple2<X, Y> = [X, Y];
+  const manush2: TGenericTuple2<string, string> = ["s", "ss"];
+
+  const user4: TGenericTuple<number, { name: string; email: string }> = [
+    12,
+    { name: "Munna", email: "m@gmail.com" },
+  ];
 }
